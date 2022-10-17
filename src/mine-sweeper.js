568a223ddd9c80b581aeba355b591056c90573cb
P1anetA <natkoch09@gmail.com>
2022-10-17 22:03:00 +0300

    Solved!

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,79 +23,145 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
+
+// npm run test
 function minesweeper(matrix) {
   let result = [];
   for (let i = 0; i < matrix.length; i++) {
-    result[i] = Array(matrix[i].length);
+    result[i] = Array(matrix[i].length);}
+  for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++){
       let count = 0;
-      if (i == 0) {
+      if (i == 0 && matrix.length > 0) {
       if (matrix[i+1][j]  == true) {
         count += 1;
       }
       }
-      if (i == matrix.length - 1) {
+      if (i == matrix.length-1 && matrix.length > 0) {
       if (matrix[i-1][j] == true){
         count += 1;
       }
-      }      
-      if (i !== 0 && i !== matrix.length - 1) {
+      }
+      if (i !== 0 && i !== matrix.length-1 && matrix.length > 0) {
       if (matrix[i+1][j] == true){
         count += 1;
-      } 
-        if ( matrix[i-1][j]  == true) {
-        count += 1;
-      }      
       }
-      if (j == 0) {
+      if (matrix[i-1][j] == true){
+        count += 1;
+      }
+    }
+      if (j == 0 && matrix[i].length > 0) {
       if ( matrix[i][j+1] == true) {
-        count += 1;
+      count += 1;
       }
       }
-      if (j == matrix[i].length - 1) {
+      if (j == matrix[i].length  && matrix[i].length > 0) {
       if ( matrix[i][j-1] == true) {
         count += 1;
       }
       }
-      if (j !== 0 && j !== matrix.length - 1) {
+      if (j !== 0 && j !== matrix.length-1 && matrix[i].length > 0) {
         if ( matrix[i][j+1] == true) {
         count += 1;
-      } 
+      }
         if (matrix[i][j-1]  == true) {
         count += 1;
-      } 
       }
-      if ()
-      
-      
-      if (matrix[i+1][j] == true){
-        count += 1;
-      } else if ( matrix[i-1][j]  == true) {
-        count += 1;
-      } else if ( matrix[i][j+1] == true) {
-        count += 1;
-      } else if (matrix[i][j-1]  == true) {
-        count += 1;
-      } else if (matrix[i+1][j+1] == true)  {
-        count += 1;
-      } else if (matrix[i+1][j-1] == true)  {
-        count += 1;
-      } else if (matrix[i-1][j+1] == true)  {
-        count += 1;
-      } else if (matrix[i-1][j-1] == true)  {
-        count += 1;
-      } 
+      }
+      if (i == 0 && j == 0 && matrix[i].length > 0) {
+        if (matrix[i+1][j+1] == true) {
+          count += 1;
+        }
+      }
+      if (i == matrix.length && j == 0 && matrix[i].length > 0) {
+        if (matrix[i-1][j+1] == true) {
+          count += 1;
+        }
+      }
+      if (i !== 0 && i !== matrix.length-1 && j == 0 && matrix[i].length > 0) {
+        if (matrix[i+1][j+1] == true){
+          count += 1;
+        }
+          if ( matrix[i-1][j+1]  == true) {
+          count += 1;
+        }
+      }
+
+      if (i == 0 && j == matrix[i].length && matrix[i].length > 0) {
+        if (matrix[i+1][j-1] == true) {
+          count += 1;
+        }
+      }
+      if (i == matrix.length - 1 && j == matrix[i].length-1 && matrix[i].length > 0) {
+        if (matrix[i-1][j-1] == true) {
+          count += 1;
+        }
+      }
+      if (i == matrix.length-1 && j == 0 && matrix[i].length > 0) {
+        if (matrix[i-1][j+1] == true) {
+          count += 1;
+        }
+      }
+      if (i !== 0 && i !== matrix.length && j == matrix[i].length && matrix[i].length > 0) {
+        if (matrix[i-1][j-1] == true) {
+          count += 1;
+        }
+        if (matrix[i+1][j-1] == true) {
+          count += 1;
+        }
+      }
+      if (i == 0 && j !== matrix[i].length && j !== 0 && matrix[i].length > 0) {
+        if (matrix[i+1][j+1] == true) {
+          count += 1;
+        }
+        if (matrix[i+1][j-1] == true) {
+          count += 1;
+        }
+      }
+      if (i == matrix.length && j !== matrix[i].length && j !== 0 && matrix[i].length > 0) {
+        if (matrix[i+1][j+1] == true) {
+          count += 1;
+        }
+        if (matrix[i+1][j-1] == true) {
+          count += 1;
+        }
+      }
+      if (i !== matrix.length-1 && i !== 0 && j !== matrix[i].length && j !== 0 && matrix[i].length > 0) {
+        if (matrix[i+1][j+1] == true) {
+          count += 1;
+        }
+        if (matrix[i+1][j-1] == true) {
+          count += 1;
+        }
+        if (matrix[i+1][j] == true) {
+          count += 1;
+        }
+        if (matrix[i-1][j] == true) {
+          count += 1;
+        }
+        if (matrix[i-1][j+1] == true) {
+          count += 1;
+        }
+        if (matrix[i-1][j-1] == true) {
+          count += 1;
+        }
+        if (matrix[i][j+1] == true) {
+          count += 1;
+        }
+        if (matrix[i][j-1] == true) {
+          count += 1;
+        }
+      }
       result[i][j] = count;
-    }    
+    }
   }
     return result;
 }
-console.log(minesweeper([
- [true, false, false],
- [false, true, false],
- [false, false, false]
-]))
-
+// console.log(minesweeper([
+//  [true, false, false],
+//  [false, true, false],
+//  [false, false, false]
+// ]))
 module.exports = {
   minesweeper
 };
